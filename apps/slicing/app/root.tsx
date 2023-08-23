c380ddcd9170aba1c42a04043e8f978a427ab80d
haikelz <haikel.2017th@gmail.com>
2023-08-23 10:56:55 +0700

    fix: mobile responsive issue

--- apps/slicing/app/root.tsx
+++ apps/slicing/app/root.tsx
@@ -22,6 +22,7 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
